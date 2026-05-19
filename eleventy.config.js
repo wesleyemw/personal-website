071d7092271707d7987708c758a14fcbaa111637
Wesley Soares <wesley.emw@gmail.com>
2026-05-19 15:57:15 -0400

    added filter to customize date

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import pluginRss from "@11ty/eleventy-plugin-rss";
+import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
   // configure image plugin
@@ -18,6 +19,10 @@ export default function (eleventyConfig) {
 
   // rss plugin configuration
   eleventyConfig.addPlugin(pluginRss);
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy.LL.dd");
+  });
 }
 
 export const config = {
